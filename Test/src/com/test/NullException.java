58a4b8dcd150aab6b5c7a3dd6ffef2c752f9b235
HAZEL <HAZEL@HAZEL>
2021-01-20 17:48:34 +0900

    21.01.20 home

---
+++ Test/src/com/test/NullException.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class NullException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String data = null;
+		
+		if(data == null) {
+			data = "";
+		}
+
+		System.out.println(data.toString());
+	}
+
+}
